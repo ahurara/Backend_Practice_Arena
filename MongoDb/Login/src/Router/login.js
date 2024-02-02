56d5541cd31b9c4119a74d12087d40602458a07f
ahurara <hurrerahchamp57@gmail.com>
2024-02-03 02:44:06 +0500

    Secured password using bcrypt and generate token for reistraion and login

--- MongoDb/Login/src/Router/login.js
+++ MongoDb/Login/src/Router/login.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = new express.Router();
 require("../db/conn");
 const register = require('../models/register');
+const brypt= require('bcryptjs');
+
 
 
 router.get('/' , (req , res)=>{
@@ -28,6 +30,9 @@ router.post("/register", async(req,res)=>{
             password : password,
             confirmPaswword : req.body.confirmpassword,
         })
+
+      const token = await registerEmployee.generateAuthToken();
+      console.log(token);
         const registered= await registerEmployee.save();
         res.render('index')
     }
@@ -52,8 +57,14 @@ router.post('/login' , async(req,res)=>{
         const email = req.body.email;
         const password = req.body.password;
         const userDetail= await register.findOne({email:email});
-
-        if(userDetail.password === password){
+        
+        if (!userDetail) {
+            return res.send("User not found");
+        }
+        const isMatch = await brypt.compare(password , userDetail.password);
+        const token = await userDetail.generateAuthToken();
+        console.log(token);
+        if(isMatch){
                 res.render('profile')
         }
         else{
--- MongoDb/Login/src/models/register.js
+++ MongoDb/Login/src/models/register.js
@@ -1,4 +1,6 @@
 const mongoose= require('mongoose');
+const bcrypt= require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const EmployeeSchema = new mongoose.Schema({
     firstname:{
@@ -30,10 +32,45 @@ const EmployeeSchema = new mongoose.Schema({
         type : String,
         required: true
     },
+    tokens : [{
+        token:{
+            type:String ,
+            required:true
+        }
+    }]
 
 
 })
 
+// generating token 
+EmployeeSchema.methods.generateAuthToken = async function (){
+    try{
+         const token = await jwt.sign({_id : this._id.toString()}  , "iamabuhurrerahfullstackdeveloper")
+         // inorder to add the token in the db
+         this.tokens = this.tokens.concat({token : token});
+         await this.save()
+        return token;
+    }
+    catch(err){
+        res.send(err)
+    }
+}
+
+// the following piece of code will run each time the data is going to save 
+ 
+
+EmployeeSchema.pre('save' , async function(next){
+
+    if(this.isModified('password')){
+       // console.log(`pass before bycrypt :${this.password}`)
+        this.password =await bcrypt.hash(this.password  , 10)
+        this.confirmPaswword =await bcrypt.hash(this.password  , 10) 
+       // console.log(`pass after bycrypt :${this.password}`)
+    }
+    
+        next();
+    
+})
 
 const register = new mongoose.model('Resgiter' , EmployeeSchema);
 
